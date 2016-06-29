79bf87694bc77f50e6009fd9d86968a1cf9e1362
Venkat Polumahanti <venkat4541@gmail.com>
2016-06-29 01:55:13 -0400

    Initial version
    Basic structure with random text and images

--- Profile website/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){ 
-    
-    $(window).stellar();
-    $("html").niceScroll();
-    
-});
---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){ 
+    
+    $(window).stellar();
+    $("html").niceScroll();
+    
+});
